fd404d0814b3ce9fe30a330dc73bd8480a9e3d41
Varunkohli018 <varunkohli18h@gmail.com>
2025-10-24 12:13:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-function runCode() {
-      const html = document.getElementById("htmlCode").value;
-      const css = "<style>" + document.getElementById("cssCode").value + "</style>";
-      const output = document.getElementById("output");
-      output.srcdoc = html + css;
-    }
-    // Auto run on load
-    runCode();
+
